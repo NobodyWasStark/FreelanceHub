d49bbdf2dc4f791063a5d21f9b88d047d2cc6fa6
miraz-ai <afzalhossain.miraz@gmail.com>
2026-05-18 15:07:56 +0600

    fixed:removed the unnecessary redundency

--- scripts/report.js
+++ scripts/report.js
@@ -7,16 +7,3 @@ function closeSidebar(){
   document.getElementById("sidebar").classList.remove("open"); 
   document.getElementById("mobile-overlay").classList.remove("show"); 
 }
-
-// CHART DATA
-let currentMode = "6m";
-const chartData = {
-  "6m": {
-    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
-    values: [320, 580, 490, 820, 1380, 1050]
-  },
-  "yr": {
-    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-    values: [320, 580, 490, 820, 1380, 1050, 1600, 1220, 980, 1450, 1800, 2100]
-  }
-};
